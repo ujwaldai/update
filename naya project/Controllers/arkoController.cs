b385b8bcb31ea9af16fa6795f6ec8cac24feb876
ujwaldai <ujwalshakha@gmail.com>
2022-03-11 08:19:07 +0545

    inital commit

---
+++ naya project/Controllers/arkoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using naya_project.Models;
+
+namespace naya_project.Controllers
+{
+    public class arkoController : Controller
+    {
+
+        daiEntities1 db = new daiEntities1();
+        // GET: arko
+        public ActionResult nayaa()
+        {
+            List<student> all_data = db.students.ToList();
+            return View(all_data );
+        }
+    }
+}
---
+++ naya project/Controllers/ujwalController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using naya_project.Models;
+namespace naya_project.Controllers
+{
+    public class ujwalController : Controller
+    {
+        daiEntities1 db = new daiEntities1();
+
+        // GET: ujwal
+        public ActionResult Index()
+        {
+
+            List<employee> all_data = db.employees.ToList();
+            return View(all_data);
+        }
+        
+    }
+}
